cc6f9601ac4acbab2d57e359cc3c3df85abbb04f
Stefani Langehennig <stefani.langehennig@colorado.edu>
2024-01-12 07:37:46 -0700

    delete .py

--- src/02-bivariate-full-plots.r
+++ src/02-bivariate-full-plots.r
@@ -12,7 +12,7 @@ full <- read_csv(here::here("data/pres-weighted-rr.csv")) # R&R data
 
 ### with legend ###
 
-p2 <- ggplot()+
+p2 <- ggplot() + 
   geom_line(data=full, aes(x=year, y=pres_no_control, color='pres_no_control', linetype='pres_no_control'),size=.82)+
   geom_line(data=full, aes(x=year, y=pres_control, color='pres_control', linetype='pres_control'),size=.82)+
   scale_x_continuous(breaks = seq(1952,2020,4)) +
