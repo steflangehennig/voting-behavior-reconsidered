c27133cf6370d7190f8a0e7f3cb3bb5422aca10f
Stefani Langehennig <stefani.langehennig@colorado.edu>
2024-01-11 19:00:15 -0700

    add code for plots

---
+++ bivariate-full-plots.r
@@ -0,0 +1,27 @@
+######################################
+# Plots for bivariate and full models
+######################################
+
+library(ggplot2)
+library(readr)
+library(ggExtra) 
+
+
+full <- read_csv(here::here("data/pres-weighted.csv")) # old data
+full <- read_csv(here::here("data/pres-weighted-rr.csv")) # R&R data
+
+### with legend ###
+
+p2 <- ggplot()+
+  geom_line(data=full, aes(x=year, y=pres_no_control, color='pres_no_control', linetype='pres_no_control'),size=.82)+
+  geom_line(data=full, aes(x=year, y=pres_control, color='pres_control', linetype='pres_control'),size=.82)+
+  scale_x_continuous(breaks = seq(1952,2020,4)) +
+  labs(x = 'Year') +
+  labs(y = 'Partisan Importance') +
+  scale_colour_manual(values = c('pres_no_control' = 'darkslategray', 'pres_control' = 'black'), 
+                      guide = 'none') +
+  scale_linetype_manual('', labels = c('Additional Controls', 'No Controls'), 
+                        values = c('pres_no_control' = 'solid', 'pres_control' = 'dashed')) +
+  theme_light()
+
+p2 +  scale_y_continuous(breaks = seq(0, 2, 0.1)) + removeGrid(y=FALSE) 
